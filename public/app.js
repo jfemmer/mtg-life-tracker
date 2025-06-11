ce6960ef192e543a8396e573ce7d9445fee7393f
jfemmer <65245024+jfemmer@users.noreply.github.com>
2025-06-10 23:42:18 -0500

    Update app.js

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (query.length < 3) return;
 
   try {
-    const res = await fetch(`https://api.scryfall.com/cards/search?q=${encodeURIComponent(query)}+is:commander`);
+    const res = await fetch(`https://api.scryfall.com/cards/search?q=${encodeURIComponent(query)}+is:commander+(type:legendary+type:creature+or+type:planeswalker)`);
     const data = await res.json();
 
     if (data.data) {
